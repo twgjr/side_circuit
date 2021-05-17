e37eac969930a1afbcb0931d7544284a7451d35c
twgjr <terry@tangibledevices.com>
2021-05-16 22:20:02 -0400

    Working on SMT solver

--- lib/solver.dart
+++ lib/solver.dart
@@ -1,3 +1,9 @@
+enum SolverState {
+  Unknown,
+  Sat,
+  Unsat
+}
+
 class Order {
   List<Map<String,String>> list;
   Order(){
@@ -20,28 +26,150 @@ class Order {
   }
 }
 
-class Solver{
+// Model:  the top level list of expression roots for the base declarations of the
+// mathematical model.  For example, dx/dt object would be declared, but does
+// not store the entire set of incremental sets of equations required to solve
+// a transient, time-valued system.  If the system is non-differential, the
+// the solver will only run with the base set. If the system contains/enabled
+// differentials, then it will generated enough equation sets to solve the
+// transient from one steady state to another.
+// During differential solve, there will be a new list of variables for each
+class Model {
   Order order = Order();
+  List<
+      Expression> expressionList; // single list of Expression roots for the Model
 
+  // points directly to all the variables in the model
+  // 0 level is base, additional levels for the differentials
+  // differential solutions are collected into the base after sat solve
+  // then diff solutions are cleared.
+  List<List<Expression>> variableList;
 
+  int solve() {
+    bool decided = false;
+    var solverState = SolverState.Unknown;
+    while (!decided) {
+      switch (solverState) {
+        case SolverState.Unknown:
+          {
+            checkModel();
+            print("Solution is unknown.");
+            break;
+          }
+        case SolverState.Sat:
+          {
+            print("Solution is sat.");
+            decided = true;
+            break;
+          }
+        case SolverState.Unsat:
+          {
+            print("Solution is UNSAT!");
+            decided = true;
+            break;
+          }
+      }
+    }
+    return solverState.index;
+  }
+
+  void checkModel(){
+
+  }
+
+    void Parenthesis() {}
+    void Equals() {}
+    void Ltoe() {}
+    void Gtoe() {}
+    void LessThan() {}
+    void GreaterThan() {}
+    void NotEquals() {}
+    void Power() {}
+    void Multiply() {}
+    void Divide() {}
+    void Add() {}
+    void Subtract() {}
 }
 
-class Model{
-  Order order = Order();
-  List<Expression> truthList;  //list of solver objects that must evaluate to true
-}
-
-// the abstract syntax tree of constants, variables, and operators
+// elements of the abstract syntax tree of a equation and it's sub-elements
 class Expression {
-  String string;  // @TODO factory for variables
-  num value; // @TODO factory for constants
-  String type;
+  String varName;  // @TODO factory for variable name
+  num value; // @TODO factory for constant value
+  Map<num,num> rangesToCheck;  //possible range of values that could be sat
+  List<num> satValues;  //range of values found to be sat
+  String type; // as defined in Order.list
   Expression parent;
   List<Expression> children;
 
   Expression(); //default expression constructor
+  Expression.parenthesis(Expression parent, Expression inside){
+    type = "Parenthesis";
+    children.add(inside);
+  }
+  Expression.equals(Expression parent, Expression exprLeft, Expression exprRight){
+    type = "Equals";
+    children.add(exprLeft);
+    children.add(exprRight);
+  }
+  Expression.ltoe(Expression parent, Expression less, Expression more){
+    type = "LTOE";
+    children.add(less);
+    children.add(more);
+  }
+  Expression.gtoe(Expression parent, Expression more, Expression less){
+    type = "GTOE";
+    children.add(more);
+    children.add(less);
+  }
+  Expression.lessThan(Expression parent, Expression less, Expression more){
+    type = "LessThan";
+    children.add(less);
+    children.add(more);
+  }
+  Expression.greaterThan(Expression parent, Expression more, Expression less){
+    type = "GreaterThan";
+    children.add(more);
+    children.add(less);
+  }
+  Expression.notEquals(Expression parent, Expression exprLeft, Expression exprRight){
+    type = "NotEquals";
+    children.add(exprLeft);
+    children.add(exprRight);
+  }
+  Expression.power(Expression parent, Expression base, Expression power){
+    type = "Power";
+    children.add(base);
+    children.add(power);
+  }
+  Expression.multiply(Expression parent, Expression exprLeft, Expression exprRight){
+    type = "Multiply";
+    children.add(exprLeft);
+    children.add(exprRight);
+  }
+  Expression.divide(Expression parent, Expression exprLeft, Expression exprRight){
+    type = "Divide";
+    children.add(exprLeft);
+    children.add(exprRight);
+  }
+  Expression.add(Expression parent, Expression exprLeft, Expression exprRight){
+    type = "Add";
+    children.add(exprLeft);
+    children.add(exprRight);
+  }
+  Expression.subtract(Expression parent, Expression exprLeft, Expression exprRight){
+    type = "Subtract";
+    children.add(exprLeft);
+    children.add(exprRight);
+  }
+  Expression.variable(Expression parent, String varName){
+    type = "Variable";
+  }
+  Expression.constant(Expression parent, String varName){
+    type = "Constant";
+  }
 }
 
+//creates Expression AST's from string's of equations or functions
 class Parser
 {
   Expression expressionGraph = Expression();  //points to the root of the abstract syntax tree
@@ -105,7 +233,7 @@ class Parser
   void makeLeaf(String matchString, String type, Expression parentNode)
   {
     Expression thisExpr = Expression();
-    thisExpr.string = matchString;
+    thisExpr.varName = matchString;
     thisExpr.type = type;
     thisExpr.parent = parentNode;
     parentNode.children.add(thisExpr);
