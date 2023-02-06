9872016e51625a1fa7ec4a10dec1f74e9be123ad
twgjr <61635412+twgjr@users.noreply.github.com>
2023-02-06 14:08:21 -0500

    refactored first jpynb and more of circuits module

--- ml/circuits.py
+++ ml/circuits.py
@@ -1,35 +1,22 @@
 import networkx as nx
 from enum import Enum
-import numpy as np
 import torch
+import random
 
 class Kinds(Enum):
     IVS = 0
-    R = 1
+    ICS = 1
+    R = 2
 
 class Props(Enum):
     I = 0
     V = 1
     Attr = 2
 
-def remove_tensor_row(tensor:torch.Tensor,row:int):
-    ''' 
-    return the tensor with the column at index "col" removed.
-
-    must be 2x1 tensor
-    '''
-    max_rows = tensor.shape[1]
-    assert(row <= max_rows)
-    if(row < max_rows):
-        return torch.cat((tensor[:row,:],tensor[row+1:,:]))
-    else:
-        return tensor[:row,:]
-
 class Circuit():
     def __init__(self) -> None:
         self.nodes: list[Node] = []
         self.elements: list[Element] = []
-        # self.ref_node: Node = None
 
     def add_node(self, element:'Element') -> 'Node':
         '''create a node for a new elemenet and add to circuit.
@@ -42,7 +29,7 @@ class Circuit():
         if(node in self.nodes):
             self.nodes.remove(node)
 
-    def add_element(self, kind=None) -> 'Element':
+    def add_element(self, kind:Kinds) -> 'Element':
         element = Element(self,kind=kind)
         high_node = self.add_node(element)
         low_node = self.add_node(element)
@@ -77,8 +64,6 @@ class Circuit():
         M_scipy = nx.incidence_matrix(G=self.nx_graph(),oriented=True)
         M_numpy = M_scipy.toarray()
         M_tensor = torch.tensor(M_numpy,dtype=dtype)
-        # assert(self.ref_node != None)
-        # return remove_tensor_row(M_tensor,self.ref_node.idx)
         return M_tensor
 
     def __repr__(self) -> str:
@@ -94,50 +79,66 @@ class Circuit():
         return parallels
 
     def extract_elements(self):
-        prop_matrix = []
-        knowns_matrix = []
-        kind_matrix = []
-        
-        num_kinds = len(Kinds)
+        '''
+        return dictinaries of circuit inputs
+        knowns map is {prop type: list(bool)} boolean list in same order as circuit
+        inputs map is {prop type: list(float)} boolean list in same order as circuit
+        kinds map is {prop type: list(bool)} boolean list in same order as circuit
+        '''
+        inputs_map = {}
+        knowns_map = {}
+        kinds_map = {}
+
+        for kind in Kinds:
+            kinds_map[kind] = []
+
+        for prop in Props:
+            inputs_map[prop] = []
+            knowns_map[prop] = []
+
         for e in range(len(self.elements)):
             element = self.elements[e]
-            properties = (element.i,element.v, element.attr)
-            num_props = len(properties)
-            kind_oh = [0]*num_kinds
-            values = [0]*num_props
-            knowns_oh = [0]*num_props
 
             for kind in Kinds:
-                if(element.kind == kind.value):
-                    kind_oh[kind.value] = 1
-
-            for p in range(num_props):
-                prop = properties[p]
-                if(prop == None):
-                    values[p] = 0.0
-                    knowns_oh[p] = 0
+                if(element.kind == kind):
+                    kinds_map[kind].append(True)
                 else:
-                    values[p] = float(prop)
-                    knowns_oh[p] = 1
+                    kinds_map[kind].append(False)
 
-            prop_matrix.append(values)
-            knowns_matrix.append(knowns_oh)
-            kind_matrix.append(kind_oh)
+            for prop in Props:
+                value = None
+                if(prop == Props.Attr):
+                    value = element.attr
+                elif(prop == Props.I):
+                    if(element.kind == Kinds.ICS):
+                        value = element.attr
+                    else:
+                        value = element.i
+                elif(prop == Props.V):
+                    print(element.kind)
+                    if(element.kind == Kinds.IVS):
+                        value = element.attr
+                    else:
+                        value = element.v
+                else:
+                    assert()
 
-        prop_tensor = torch.tensor(prop_matrix)
-        knowns_tensor = torch.tensor(knowns_matrix)
-        kind_tensor = torch.tensor(kind_matrix)
-            
-        return (kind_tensor, prop_tensor, knowns_tensor)
+                if(value == None):# unknown
+                    inputs_map[prop].append(random.random()) # initialize unknowns
+                    knowns_map[prop].append(False)
 
+                else: # known
+                    inputs_map[prop].append(float(value))
+                    knowns_map[prop].append(True)
+
+        return kinds_map, inputs_map, knowns_map
+        
     def extract_nodes(self):
         pot_tensor = []
         knowns_tensor = []
 
         for n in range(len(self.nodes)):
             node = self.nodes[n]
-            # if(node == self.ref_node):
-            #     continue
             values = 0
             knowns_oh = 0
             if(node.p == None):
@@ -184,16 +185,13 @@ class Node():
         if(element not in self.elements):
             self.elements.append(element)
 
-    def set_ground(self):
-        self.p = 0
-        self.circuit.set_ground(self)
-
 class Element():
-    def __init__(self, circuit: Circuit, low:Node = None, high:Node = None, 
-                kind = None, v = None, i = None, attr = None) -> None:
+    def __init__(self, circuit: Circuit, kind:Kinds, low:Node = None, high:Node = None,
+                 v = None, i = None, attr = None) -> None:
+        assert(isinstance(kind,Kinds))
         self.circuit = circuit
-        self.low:Node = low
-        self.high:Node = high
+        self.low = low
+        self.high = high
         self.kind = kind
         self.i = i
         self.v = v
@@ -207,7 +205,6 @@ class Element():
         v = ('v',self.v)
         i = ('i',self.i)
         attr = ('attr',self.attr)
-        # from node, to node, edge key, attributes
         return (self.low.idx, self.high.idx, self.key, (kind, i, v, attr))
 
     @property
@@ -239,37 +236,17 @@ class Input():
     def __init__(self, circuit:Circuit) -> None:
         self.circuit = circuit
         self.M = self.circuit.M()
+        self.kinds_map, self.inputs_map, self.knowns_map = self.circuit.extract_elements()
 
-    def mask(self,tensor,column):
+    def get_col(self,tensor:torch.Tensor,column:torch.Tensor):
+        '''returns any 1D column of elements from a 2D tensor given a 1 D column
+         boolean mask'''
         return tensor[:,column].reshape(self.circuit.num_elements(),1)
-        
-    def known_masks(self):
-        _, _, e_knowns_tensor = self.circuit.extract_elements()
-        _, n_knowns_tensor = self.circuit.extract_nodes()
-        
-        current = self.mask(e_knowns_tensor,Props.I.value)
-        voltage = self.mask(e_knowns_tensor,Props.V.value)
-        pot = n_knowns_tensor
-        attr = self.mask(e_knowns_tensor,Props.Attr.value)
-
-        return (current, voltage, pot, attr)
-
-    def kind_map(self):
-        kind_map = {}
-        
-        kind_map[Kinds.IVS.value] = self.is_kind_mask(Kinds.IVS)
-        kind_map[Kinds.R.value] = self.is_kind_mask(Kinds.R)
-
-        # kind_map[Kinds.IVS.value].requires_grad=True
-        # kind_map[Kinds.R.value].requires_grad=True
-
-        return kind_map
 
     def rs_mask(self, kind_map):
         num_elem = self.circuit.num_elements()
         
-        # is source
-        s_mask = kind_map[Kinds.IVS.value]
+        s_mask = kind_map[Kinds.IVS]
         is_s_mask_m = s_mask @ s_mask.T
         is_s_mask_y = torch.cat(tensors= (
                             torch.zeros(size=(num_elem,num_elem)),
@@ -277,8 +254,7 @@ class Input():
                             torch.zeros(size=(num_elem,num_elem))
                         ),dim=1)
 
-        # is resistor
-        r_mask = kind_map[Kinds.R.value]
+        r_mask = kind_map[Kinds.R]
         is_r_mask_m = r_mask @ r_mask.T
         is_r_mask_z = torch.cat(tensors= (
                             is_r_mask_m,
@@ -294,39 +270,16 @@ class Input():
 
         return is_r_mask_z, is_r_mask_y, is_s_mask_y
 
-
-    def is_kind_mask(self,kind:Kinds):
-        element_kinds, _, _ = self.circuit.extract_elements()
-        is_kind = element_kinds[:,kind.value].to(torch.float)
-        is_kind = is_kind.reshape(self.circuit.num_elements(),1)
-        # is_kind.requires_grad=True
-        # print(is_kind)
-        return is_kind
-
-    def z(self):
-        pass
-
-    def y(self):
-        pass
-
     def init_tensors(self):
         num_elem = self.circuit.num_elements()
         num_nodes = self.circuit.num_nodes()
-        pot_tensor,_ = self.circuit.extract_nodes()
-        pot_tensor = pot_tensor.reshape(num_nodes,1)
-        _,element_props,_ = self.circuit.extract_elements()
-        # shape inputs
-        i_tensor = element_props[:,Props.I.value].reshape(num_elem,1)
-        v_tensor = element_props[:,Props.V.value].reshape(num_elem,1)
-        attr_tensor = element_props[:,Props.Attr.value].reshape(num_elem,1)
-        # init unknown attr
-        _,_,_,attr_mask = self.known_masks()
-        attr_mask = attr_mask.to(torch.bool)
-        attr_tensor[~attr_mask] = 1
+        pot_tensor = torch.rand(size=(num_nodes,1))
+        i_tensor = torch.tensor(self.inputs_map[Props.I]).reshape(num_elem,1)
+        v_tensor = torch.tensor(self.inputs_map[Props.V]).reshape(num_elem,1)
+        attr_tensor = torch.tensor(self.inputs_map[Props.Attr]).reshape(num_elem,1)
         return i_tensor, v_tensor, pot_tensor, attr_tensor
 
     def element_row(self, element_attrs):
-        # _,_,_,element_attrs = self.init_tensors()
         mask_rz,mask_ry,mask_sy = self.rs_mask(self.kind_map())
         z_r = mask_rz * -element_attrs
         y_r = mask_ry
@@ -337,7 +290,6 @@ class Input():
     def s(self):
         _,_,_,element_attrs = self.init_tensors()
         kind_map = self.kind_map()
-        # i_src = None
         s = torch.zeros_like(element_attrs)
         v_bool = kind_map[Kinds.IVS.value].to(torch.bool)
         v_src = element_attrs[v_bool].unsqueeze(dim=1)
