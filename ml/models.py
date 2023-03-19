6985ce568c6d0198b88904e6c9103818fbff6fd8
twgjr <61635412+twgjr@users.noreply.github.com>
2023-03-19 13:59:16 -0400

    Split main module into submodules.

--- ml/models.py
+++ ml/models.py
@@ -5,18 +5,91 @@ from circuits import Props,Kinds
 from data import Data
 from torch.linalg import solve
 
+class Z(nn.Module):
+    def __init__(self, r_mask: Tensor, ics_mask: Tensor):
+        super().__init__()
+        self.r_mask = r_mask
+        self.ics_mask = ics_mask
+    
+    def forward(self,attr):
+        Z_r = torch.diag(attr) @ torch.diag(self.r_mask).to(torch.float)
+        Z_ics = torch.diag(self.ics_mask)
+        return -Z_r + Z_ics
+
+class Y(nn.Module):
+    def __init__(self, r_mask: Tensor, ivs_mask: Tensor):
+        super().__init__()
+        self.Y_r = torch.diag(r_mask)
+        self.Y_ivs = torch.diag(ivs_mask)
+    
+    def forward(self):
+        return self.Y_r + self.Y_ivs
+
+class E(nn.Module):
+    def __init__(self, r_mask: Tensor, ics_mask: Tensor, ivs_mask: Tensor):
+        super().__init__()
+        self.Z = Z(r_mask,ics_mask)
+        self.Y = Y(r_mask,ivs_mask)
+        
+    def forward(self,attr):
+        return torch.cat(tensors=(self.Z(attr),self.Y()),dim=1)
+
+class A(nn.Module):
+    def __init__(self, data: Data, r_mask: Tensor, ics_mask: Tensor, 
+                 ivs_mask: Tensor):
+        super().__init__()
+        M_zeros = torch.zeros_like(data.M)
+        kvl_coef = torch.tensor(data.circuit.kvl_coef()).to(torch.float)
+        kvl_zeros = torch.zeros_like(kvl_coef)
+        self.kcl = torch.cat(tensors=(data.M,M_zeros),dim=1)
+        self.kvl = torch.cat(tensors=(kvl_zeros,kvl_coef),dim=1)
+        self.elements = E(r_mask,ics_mask,ivs_mask)
+    
+    def forward(self,attr):
+        return torch.cat(tensors=(self.kcl,self.kvl,self.elements(attr)), dim=0)
+
+class S(nn.Module):
+    def __init__(self, ics_mask: Tensor, ivs_mask: Tensor):
+        super().__init__()
+        self.ics_mask = ics_mask
+        self.ivs_mask = ivs_mask
+
+    def forward(self,attr):
+        return attr * torch.logical_or(self.ics_mask,self.ivs_mask)
+    
+class B(nn.Module):
+    def __init__(self, data: Data, ics_mask: Tensor, ivs_mask: Tensor):
+            super().__init__()
+            num_nodes = data.circuit.num_nodes()
+            num_elements = data.circuit.num_elements()
+            self.kcl = torch.zeros(num_nodes)
+            self.kvl = torch.zeros(num_elements - num_nodes + 1)
+            self.sources = S(ics_mask,ivs_mask)
+    
+    def forward(self,attr):
+        s = self.sources(attr)
+        b = torch.cat(tensors=(self.kcl,self.kvl,s), dim=0)
+        return b.unsqueeze(dim=1)
+
 class Solver(nn.Module):
     def __init__(self, data: Data):
         super().__init__()
+        #data
         self.data = data
         self.ics_attr_mask = self.init_mask(Kinds.ICS)
         self.ivs_attr_mask = self.init_mask(Kinds.IVS)
         self.r_attr_mask = self.init_mask(Kinds.R)
         self.known_attr_mask = self.init_known_attr_mask()
-        (self.i_base,
-        self.v_base,
-        self.r_base) = self.data.init_base()
+        self.i_base, self.v_base, self.r_base = self.data.init_base()
+        #model
         self.attr = nn.Parameter(self.init_attr())
+        self.A = A(data,self.r_attr_mask,self.ics_attr_mask,self.ivs_attr_mask)
+        self.b = B(data,self.ics_attr_mask,self.ivs_attr_mask)
+
+    def forward(self):
+        A = self.A(self.attr)
+        b = self.b(self.attr)
+        return solve(A[1:,:],b[1:,:])
 
     def init_mask(self, kind:Kinds):
         return torch.tensor(self.data.mask_of_kind(kind)).to(torch.bool)
@@ -71,10 +144,6 @@ class Solver(nn.Module):
         attr[self.r_attr_mask] = self.denorm(attr[self.r_attr_mask],self.r_base)
         return attr
     
-    def forward(self):
-        A,b = self.build()
-        return solve(A[1:,:],b[1:,:])
-    
     def zero_known_grads(self):
         if(self.attr != None and self.attr.grad != None):
             self.attr.grad[self.known_attr_mask] = 0
@@ -84,52 +153,6 @@ class Solver(nn.Module):
         max = 1e15
         for p in self.parameters():
             p.data.clamp_(min, max)
-
-    def M(self):
-        return self.data.M
-
-    def build(self):
-        num_nodes = self.data.circuit.num_nodes()
-        kcl_coef = self.M()
-        M_zeros = torch.zeros_like(kcl_coef)
-        kcl_block = torch.cat(tensors=(kcl_coef,M_zeros),dim=1)
-        kvl_coef = torch.tensor(self.data.circuit.kvl_coef()).to(torch.float)
-        kvl_zeros = torch.zeros_like(kvl_coef)
-        kvl_block = torch.cat(tensors=(kvl_zeros,kvl_coef),dim=1)
-        element_coef = self.E()
-        A = torch.cat(tensors=(
-                kcl_block,
-                kvl_block,
-                element_coef
-            ), dim=0)
-        kcl_zeros = torch.zeros(num_nodes)
-        kvl_zeros = torch.zeros(kvl_block.shape[0])
-        s_const = self.source_attr()
-        b = torch.cat(tensors=(
-                kcl_zeros,
-                kvl_zeros,
-                s_const
-            ), dim=0)
-        return A,b.unsqueeze(dim=1)
-    
-    def E(self):
-        Z = self.Z()
-        Y = self.Y()
-        return torch.cat(tensors=(Z,Y),dim=1)
-
-    def source_attr(self):
-        source_mask = torch.logical_or(self.ics_attr_mask,self.ivs_attr_mask)
-        return self.attr * source_mask
-
-    def Z(self):
-        Z_r = torch.diag(self.attr) @ torch.diag(self.r_attr_mask).to(torch.float)
-        Z_ics = torch.diag(self.ics_attr_mask)
-        return -Z_r + Z_ics
-
-    def Y(self):
-        Y_r = torch.diag(self.r_attr_mask)
-        Y_ivs = torch.diag(self.ivs_attr_mask)
-        return Y_r + Y_ivs
     
     def set_r_from_knowns(self, preds:Tensor,target:Tensor,target_mask:Tensor):
         with torch.no_grad():
@@ -145,4 +168,3 @@ class Solver(nn.Module):
             i[i_known_mask] = i_known[i_known_mask]
             v[v_known_mask] = v_known[v_known_mask]
             self.attr[self.r_attr_mask] = v[self.r_attr_mask]/i[self.r_attr_mask]
-
